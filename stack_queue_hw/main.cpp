05b65ddeedc367fcaa909fda6c6b2933ae83af96
Mileno Valdo <milenovaldo@icloud.com>
2020-03-17 22:28:35 +0700

    Fauzan senpai help

--- stack_queue_hw/main.cpp
+++ stack_queue_hw/main.cpp
@@ -7,14 +7,14 @@ https://github.com/sunnyjovita
 
 #include <iostream>
 #include "headers/stonks.h"
-#include "headers/easyio.h"
 
 using namespace std;
 
+
 int main(void){
 
-    Stock stock_list[] = {};
-    int list_index = 0;
+    // Stock stock_list[] = {};
+    // int list_index = 0;
 
     while(1){
         cout <<
@@ -23,16 +23,23 @@ int main(void){
         "2) Find LIFO and FIFO price of a stonk.\n" <<
         "Select an option: ";
 
-        int selection = scan<int>();
+        int selection;
+        cin >> selection;
 
         if (selection == 1){
-            stock_list[list_index] = new_stock();
+            // new_stock();
+            add_node();
         }else if(selection == 2){
-            return;
+            current_pointer = head;
+            while(current_pointer != NULL){
+                cout << "[" << "|" << current_pointer->next << "]----->";
+                current_pointer = current_pointer->next;
+            }
+            cout << "NULL" << endl;
         }else{
-            print("Invalid selection");
+            cout << "Invalid selection" << endl;
         }
-    return 0;
+        
     }
-    
+    return 0;
 }
